ce5ef6abd831d0ae044a61b4a5cb78c6e6d17cfb
HaroldScore <sancorharold@gmail.com>
2025-11-18 00:22:20 -0500

    subete mrda

--- proy_vbc/urls.py
+++ proy_vbc/urls.py
@@ -23,5 +23,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("core.urls", namespace="core")),
-    path("", include("commerce.urls", namespace="commerce")),
+    path("commerce/", include("commerce.urls", namespace="commerce")),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
